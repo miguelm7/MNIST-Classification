e7cd07613c975068c9a411584dcc1a6f6f73300a
Miguel Moreno Rodríguez <miguelmorenorodriguez@MacBook-Air-de-Miguel.local>
2021-02-04 21:24:40 +0100

    importar paquetes de keras

--- mnist.py
+++ mnist.py
@@ -1,5 +1,13 @@
 
-print("Hola Mundo")
+#source ./venv/bin/activate 
 
+import keras
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+print("Hola Mundo")
 
   
